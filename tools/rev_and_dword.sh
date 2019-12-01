4d08c91f248474b40606d5b01b59b7777294ed87
Chase Hatch <chase.hatch.business@gmail.com>
2019-11-30 19:46:33 -0800

    updated to reflect end-of-exam version

--- tools/rev_and_dword.sh
+++ tools/rev_and_dword.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-# Helper for reversing and dwording shellcode; useful for figuring out what targets are for carving values out on the stack using calc_targets.py.
-## Example usage:
-# ./odsc egg.o  |tail -n 2 |head -n 1 |echo -ne "$(cat -)" | ./rev_and_dword.sh 
-# e7ffe775
-# afea75af
-# d7895730
-# 3054b8ef
-# 745a053c
-# 2ecd5802
-# 6a52420f
-# ffca8166
-## You will note that the output is the egghunter, reversed, in DWORDs.  From top to bottom are the values that would need to be carved out and pushed onto the stack.
-## Takes either stdin or first argument, but must be raw bytes either way.
-
-
 
 NARGS=$#
 
-if [[ $NARGS -ge 1 ]]; then 
-	echo -ne "$1" | xxd -c 1 -g 1 |sed -re 's/[0-9a-fA-F]{3,8}: //g' -e 's/\ .*//g' |tac |tr '\n' ' ' | xxd -r -p |xxd -c 4 -g 4 |sed -re 's/[0-9a-fA-F]{7}: //g' -e 's/ .*//g'
+if [[ $NARGS -ge 1 ]]; then
+	if [[ -f $1 ]]; then
+		# is file
+		cat $1 |echo -ne "$(cat -)"
+	else
+		# is argument
+		echo -ne "$1"
+	fi
 else
-	cat - | xxd -c 1 -g 1 |sed -re 's/[0-9a-fA-F]{3,8}: //g' -e 's/\ .*//g' |tac |tr '\n' ' ' | xxd -r -p |xxd -c 4 -g 4 |sed -re 's/[0-9a-fA-F]{7}: //g' -e 's/ .*//g'
-fi
-
+	# input comes from pipe?
+	echo -ne "$(cat -)"
+fi  |\
+	xxd -c 1 -g 1 |\
+	sed -re 's/[0-9a-fA-F]{3,8}: //g' -e 's/\ .*//g' |\
+	tac |\
+	tr '\n' ' ' |\
+	xxd -r -p |\
+	xxd -c 4 -g 4 |\
+	sed -re 's/[0-9a-fA-F]{7}: //g' -e 's/ .*//g'
 
